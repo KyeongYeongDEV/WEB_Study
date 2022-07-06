7e64a83b015ec73f384ce137363e0b78cc2f0fd6
KyeongYeongDEV <yeong7098@naver.com>
2022-07-06 10:18:01 +0900

    Done

--- Python_스크래핑/practice/7_bs4_coupang.py
+++ Python_스크래핑/practice/7_bs4_coupang.py
@@ -12,7 +12,28 @@ soup = BeautifulSoup(res.text,"lxml")
 items = soup.find_all("li", attrs={"class":re.compile("^search_product")})
 
 for item in items:
+    #광고 제품은 제외
+    ad_badge = item.find("span",attrs={"class": "ad-badge-text"})
+    if ad_badge:
+        print(" <광고 상품 제외합니다.>")
+        continue
+
+    #리뷰 100개이상, 평점 4.5이상 되는 것만 조회
+    rate = item.find("em",attrs={"class":"rating"})#평점
+    if rate:
+        rate = rate.get_text()
+    else:
+        rate = "평점없음"
+        print(" <평점 없는 상품 제외합니다.>")
+        continue
+
     name = item.find("div", attrs={ "class":"name"}).get_text() #제품명
+
+    #애플 상품 제외
+    if "Apple" in name:
+        print("    <Apple 상품 제외합니다.>")
+        continue
+
     price = item.find("strong", attrs={"class":"price-value"}).get_text() #가격 정보
     rate = item.find("em", attrs={"class":"rating"}) #평점
     if rate:
@@ -20,10 +41,15 @@ for item in items:
     else:
         rate = "평점 없음"
 
-    rate_cnt = item.find("span", attrs={"class":"rating-total-count"})
+
+    rate_cnt = item.find("span", attrs={"class":"rating-toal-count"})
     if rate_cnt:
         rate_cnt = rate.get_text()
+        rate_cnt = rate_cnt[1:-1]
     else:
-        rate_cnt = "평점 수 없음"
+        rate_cnt = "    <평점 수 없음 상품 제외합니다.>"
+        continue
      
-    print(name, price, rate, rate_cnt)
+    if float(rate) >= 4.5 and int(rate_cnt) >= 50:
+        print(name, price, rate, rate_cnt)
+    
---
+++ Python_스크래핑/practice/깃허브커밋비교.py
@@ -0,0 +1,26 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+user1_url = "https://github.com/KyeongYeongDEV"
+user1_res = requests.get(user1_url)
+user1_res.raise_for_status()
+user1_soup = BeautifulSoup(user1_res.text,"lxml")
+user1_item = user1_soup.find("h2", attrs={"class":"f4 text-normal mb-2"}).get_text()
+user1_commit = int(user1_item[7:10])
+
+user2_url = "https://github.com/min-0"
+user2_res = requests.get(user2_url)
+user2_res.raise_for_status()
+user2_soup = BeautifulSoup(user2_res.text,"lxml")
+user2_item = user2_soup.find("h2", attrs={"class":"f4 text-normal mb-2"}).get_text()
+user2_commit = int(user2_item[7:10])
+
+
+print("user1의 커밋 : " + str(user1_commit))
+print("user2의 커밋 : " + str(user2_commit))
+print("더 많이 커밋한 사람은 >> " , end="")
+if user1_commit > user2_commit:
+    print("user1")
+else:
+    print("user2")
