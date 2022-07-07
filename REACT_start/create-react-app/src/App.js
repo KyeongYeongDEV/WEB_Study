7879a6e80847f108dcc44101e8fe2779bd280b72
KyeongYeongDEV <yeong7098@naver.com>
2022-07-07 17:38:01 +0900

    Done

--- REACT_start/create-react-app/src/App.js
+++ REACT_start/create-react-app/src/App.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
-import TOC from "./components/TOC"
-import Content from "./components/Content"
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 import Subject from './components/Subject';
 import './App.css';
-
-
-
 
 class App extends Component{
   render(){
     return (
-      <div className = "App">
-        <Subject title = "WEB" sub = "world wide wed!"></Subject>
-        <Subject title = "React" sub = "For UI"></Subject>
-        <TOC></TOC>
-        <Content title = "HTML" desc= "HTML us HyperText Markup Language."></Content>
+        <div className = "App">
+          <Subject 
+            title = "WED"
+            sub ="World wide Web!">
+          </Subject>
+          <Subject 
+            title = "React" 
+            sub = "For UI">
+          </Subject>
+          <TOC></TOC>
+          <Content title = "HTML" desc= "HTML us HyperText Markup Language."></Content>
       </div>
     );
   }
