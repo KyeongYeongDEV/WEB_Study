89b8e9fe0dd08cde2d4ee9839c84a3347544d525
KyeongYeongDEV <yeong7098@naver.com>
2022-07-19 10:41:08 +0900

    Done

---
+++ REACT_start/begin/0719_coding_acma/src/component/Hello.js
@@ -0,0 +1,26 @@
+
+export default function Hello(){
+function showName(){
+   console.log('Mike');
+}
+
+function showAge(age){
+   console.log(age);
+}
+
+   return (
+      <div>
+         <h1>Hello</h1>
+         <button onClick={showName }>Show name</button>
+         <button onClick={()=>{
+               showAge(10);
+            }}>
+               Show age
+            </button>
+            <input type = 'text' onChange={(e)=>{
+               console.log(e.target.value)
+            }}></input>
+
+      </div>
+   );
+} 
