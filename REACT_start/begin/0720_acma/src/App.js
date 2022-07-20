620e24a312d14e8d8dcab59a01294ca051e6d4a5
KyeongYeongDEV <yeong7098@naver.com>
2022-07-20 14:33:34 +0900

    Done

--- REACT_start/begin/0720_acma/src/App.js
+++ REACT_start/begin/0720_acma/src/App.js
@@ -1,10 +1,24 @@
+import DayList from "./conponent/DayList";
 import Header from "./conponent/Header";
+import Day from './conponent/Day';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header />
+      <Switch>
+        <Route exact path = '/'>
+          <DayList />
+        </Route>
+
+        <Route path = '/day:day'>
+          <Day />
+        </Route>
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
--- REACT_start/begin/0720_acma/src/conponent/Header.js
+++ REACT_start/begin/0720_acma/src/conponent/Header.js
@@ -1,8 +1,11 @@
+import reactRouterDom from "react-router-dom";
+import {Link} from 'react-router-dom';
+
 export default function Header(){
     return(
         <div className="header">
             <h1>
-                <a href ='/'>토익 영어단어(고급)</a>
+                <Link to ='/'>토익 영어단어(고급)</Link>
             </h1>
             <div className="menu">
                 <a href="#x" className = "link">
