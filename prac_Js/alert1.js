f0b32e959336cf94a0621b988ed33810044afe14
KyeongYeongDEV <yeong7098@naver.com>
2022-07-22 16:16:10 +0900

    Done

--- prac_Js/alert1.js
+++ prac_Js/alert1.js
@@ -1,2 +1,10 @@
-alert("에러가 발생합니다.");
-[1,2].forEach(alert)
+// let age = prompt("나이를 입력해주세요", 100); //입력칸 있음
+// alert(`당신의 나이는 ${age}입니다.`)
+
+//[1,2].forEach(alert)
+
+// let text = confirm("당신은 주인인가요?"); //예 아니요 있음
+// alert(test)
+
+let name = prompt("사용자의 이름은 무엇입니까?",'홍길동');
+alert(`사용자 이름 : ${name}`);
