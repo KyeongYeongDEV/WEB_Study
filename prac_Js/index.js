0934a076c70880d5c989299ac54b7aec60e10d21
KyeongYeongDEV <yeong7098@naver.com>
2022-08-02 11:57:56 +0900

    Done

--- prac_Js/index.js
+++ prac_Js/index.js
@@ -384,15 +384,15 @@
 
 //----------------------- 
 //symbol
-let user={
-    name: john,
-    money = 1000,
-    [Symbol.toPrimitive](hint){
-        alert(`hint: ${hint}`);
-        return hint == "string" ? `{name : "${this.name}"}` : this.money;
+// let user={
+//     name: john,
+//     money = 1000,
+//     [Symbol.toPrimitive](hint){
+//         alert(`hint: ${hint}`);
+//         return hint == "string" ? `{name : "${this.name}"}` : this.money;
 
-    }
-};
-alert (user);
-alert(+user);
-alert(user +500);
+//     }
+// };
+// alert (user);
+// alert(+user);
+// alert(user +500);
