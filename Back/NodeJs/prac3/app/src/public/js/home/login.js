4c7ac392b129cf908d5da0122ec3111f764801cd
KyeongYeongDEV <yeong7098@naver.com>
2022-08-17 17:09:47 +0900

    Done

--- Back/NodeJs/prac3/app/src/public/js/home/login.js
+++ Back/NodeJs/prac3/app/src/public/js/home/login.js
@@ -10,6 +10,14 @@ function login(){
     const req={
         id : id.value,
         psword: psword.value,
-    }
-    console.log(req);
+    };
+
+    fetch("/login",{//fetch는 객체형태로 값을 줘야 함
+        method: "POST",//rest API 형태
+        header:{//
+            "Content-Type" : "applicationjson" //내가 보내는 데이터 타입 명시
+        },
+        body: JSON.stringify(req),//오브젝트 형태를 json 형태로 바꿔준다 = 문자열(" ")로 감싸져 있다
+
+    });
 }
