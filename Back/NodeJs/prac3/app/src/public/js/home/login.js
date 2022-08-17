9c62ae5e63b197c610e70e1e2b334f963e182f63
KyeongYeongDEV <yeong7098@naver.com>
2022-08-17 15:58:11 +0900

    DOM으로 HTML 객체 제어하기

--- Back/NodeJs/prac3/app/src/public/js/home/login.js
+++ Back/NodeJs/prac3/app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use-script";
 
-console.log("hello");
+const id = document.querySelector("#id"), // '#'을 붙이므로 태그에 id의 id를 가져와달라
+  psword = document.querySelector("#password"),//태그의 id의 password를 가져와달라
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req={
+        id : id.value,
+        psword: psword.value,
+    }
+    console.log(req);
+}
