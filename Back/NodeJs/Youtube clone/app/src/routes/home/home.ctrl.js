c7a541141707b37167563a88bd39225c5d3f736f
KyeongYeongDEV <yeong7098@naver.com>
2022-08-23 17:40:37 +0900

    Done

--- Back/NodeJs/Youtube clone/app/src/routes/home/home.ctrl.js
+++ Back/NodeJs/Youtube clone/app/src/routes/home/home.ctrl.js
@@ -20,23 +20,19 @@ const process ={
         const id = req.body.id,
             psword = req.body.psword;
 
+        const response ={};
         if(users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if(users.psword[idx] === psword){
-                return res.json({
-                    success: true,
-                });
+                response.success = true; // key = success, value = true;
+                return res.json(response);
             }
         }
-
-        return res.json({
-            success: false,
-            msg: "로그인에 실패하였습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패하셨습니다."
+        return res.json(response); 
     },
 };
-
-
 
 module.exports = { //내보낼 때 이름 같에 하기
    output, process,
