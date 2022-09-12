086a23f9b52051214169e57212d2259b8650b247
KyeongYeongDEV <yeong7098@naver.com>
2022-09-12 20:40:49 +0900

    Done

--- Back/Nodejs Furthemore/크롤링2/main.js
+++ Back/Nodejs Furthemore/크롤링2/main.js
@@ -10,25 +10,25 @@ const getHTML = async()=>{
         //Node.jd 서버에서 외부의 웹브라우저에 요청해서 특정 HTML을 내려받기 위해 필요한 패키지
         return await axios.get(URL);
     }catch(err){
-        console.error(err); //error 발생시 콘솔에 띄워줌
+        console.error(err); 
     }
 };
 
 const prasing = async()=>{
     const html = await getHTML();
     //cheerio
-    //가져온 HTML에서 특정 위히(depth)를 가져오기 위해 필요한 패키지
+    //가져온 HTML에서 특정 위치(depth)를 가져오기 위해 필요한 패키지
     //load()
     //인자로 html 문자열을 받아 cheerio 객체 반환
-    const $ = cheerio.load(html.data);
-    const $RankList = $(".teamRankTabPanel_0");
+    const $ = cheerio.load(html.data); //JQuery
+    const $RankList = $("div.teamRankTabPanel_0.tmp_wrap tabgroup3");
 
     let Rankes = [];
     $RankList.each((idx, node)=>{
-        const title  = $(node).find(".teamRankTabPanel_0").text();
+        const title  = $(node).find("div.teamRankTabPanel_0.tmp_wrap tabgroup3").text();
         Rankes.push({
-            rank:$(node).find("row"),
-            team: $(node).find("tboby tr td team p span a")
+            //rank:$(node).find("th"),
+            team: $(node).find("table.tb_type2 tbody tr th").text(),
         })
     });
     console.log(Rankes);
