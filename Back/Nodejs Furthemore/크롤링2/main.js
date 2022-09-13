d1dfa8355b4987c154d8408940824275bd833703
KyeongYeongDEV <yeong7098@naver.com>
2022-09-13 22:13:24 +0900

    Done

--- Back/Nodejs Furthemore/크롤링2/main.js
+++ Back/Nodejs Furthemore/크롤링2/main.js
@@ -2,37 +2,36 @@
 const axios = require("axios");
 const cheerio= require("cheerio");
 
-const URL  = "https://search.naver.com/search.naver?sm=tab_sug.top&where=nexearch&query=%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84&oquery=%EC%98%81%ED%99%94&tqi=hyty1sprvTossEk5Ovosssssssl-009660&acq=%EC%98%81%ED%99%94&acr=1&qdt=0";
+const URL  ="https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=";
 
-const getHTML = async()=>{
+const getHTML = async(keyword)=>{
     try{
         //axios 
         //Node.jd 서버에서 외부의 웹브라우저에 요청해서 특정 HTML을 내려받기 위해 필요한 패키지
-        return await axios.get(URL);
+        return await axios.get(URL + encodeURI(keyword));
     }catch(err){
         console.error(err); 
     }
 };
 
-const prasing = async()=>{
-    const html = await getHTML();
+const prasing = async(keyword)=>{
+    const html = await getHTML(keyword);
     //cheerio
     //가져온 HTML에서 특정 위치(depth)를 가져오기 위해 필요한 패키지
     //load()
     //인자로 html 문자열을 받아 cheerio 객체 반환
     const $ = cheerio.load(html.data); //JQuery
-    const $NameList = $(".title_box");
+    const $NameList = $(".data_box");
 
     let titles = [];
     let count=0;
     $NameList.each((idx, node)=>{
         titles.push({
             rank: ++count,
-            name: $(node).find(".name").text(),
+            name: $(node).find(".area_text_box").text(),
         });
-        if(this.rank == 15){return ;}
     });
     console.log(titles);
 };
 
-prasing();
+prasing("현재상영영화");
