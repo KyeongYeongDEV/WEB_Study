3d73d4cd357dff17d3c8abf9bdee2738c098499e
KyeongYeongDEV <yeong7098@naver.com>
2022-10-30 16:04:10 +0900

    controller findUser

---
+++ Back/코딩애미 과제/연습/controller/user.js
@@ -0,0 +1,17 @@
+const db = require('../config/db')
+const dotenv = require('dotenv').config()
+
+module.exports ={
+    findUser: (username) => new Promise((resolve, reject)=>{
+        const sql = `select *from "${process.env.DB_TABLE}" where name = "${username}}"`
+        db.query(sql,(err,result)=>{
+            if(err) reject({msg:'false'})
+            if(result.length){
+                resolve({
+                    msg: 'true',
+                    username:result[0].name
+                })
+            }
+        })
+    })
+}
