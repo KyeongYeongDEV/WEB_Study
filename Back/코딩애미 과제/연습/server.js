ddd8602c8e87ea6a08bf6373112fb8974fb0a60e
KyeongYeongDEV <yeong7098@naver.com>
2022-10-30 14:56:52 +0900

    server

--- Back/코딩애미 과제/연습/server.js
+++ Back/코딩애미 과제/연습/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const Dotenv = require('dotenv')
 
 app.get('/',(req, res)=>{
-    res.send('HIIIIIII')
+    res.send('HIIIIII')
 })
 
 app.listen(3000, ()=>{
