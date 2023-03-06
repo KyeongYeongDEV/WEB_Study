5cd66d05535ab78ce1af9a7480d6ebf8726ea01f
KyeongYeongDEV <yeong7098@naver.com>
2023-03-06 14:33:59 +0900

    study: sort 수정

--- Back/코딩애미 과제/10.물류 관리/src/controller/sort.js
+++ Back/코딩애미 과제/10.물류 관리/src/controller/sort.js
@@ -2,10 +2,7 @@ class Sort{
     listSort(list){
         return list.sort((a,b)=>{ 
             if(a.deadline > b.deadline) return 1
-            else if(a.deadline < b.deadline) return -1
-            else if(a.deadline == b.deadline && a.quantity >= b.quantity) return 1
-            else return -1
-            
+            else if(a.deadline < b.deadline || a.quantity >= b.quantity) return -1
         })
     }
     productTimeSort(tmpProductTimeList){
