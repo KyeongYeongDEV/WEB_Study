d80f7535116c4de854a604b057af65363f4bed11
KyeongYeongDEV <yeong7098@naver.com>
2023-03-07 14:45:55 +0900

    study : toStrictEqual()

--- Back/jest/src/fn.test.js
+++ Back/jest/src/fn.test.js
@@ -1,16 +1,27 @@
 const fn = require("./fn")
 
 test('이름과 나이를 전달 받아서 객체를 반환해줘', ()=>{
-    expect(fn.makeUser("Mike", 30)).toBe({
+    expect(fn.makeUser("Mike", 30)).toEqual({ //gender가 test에 없음에도 테스트가 통과한다.
         name :"Mike",
         age : 30,
     }) 
-    //조건을 같게 해줘도 객체는 재귀형식으로 검사를 해야 함으로 아래와 같이 해줘야 한다.
+    
 })
 
 test('이름과 나이를 전달 받아서 객체를 반환해줘', ()=>{
-    expect(fn.makeUser("Mike", 30)).toEqual({ //toBe가 아니라 toEqual() 해줘야 한다.
+    expect(fn.makeUser("Mike", 30)).toStrictEqual({ //좀 더 엄격하게 테스트를 하고 싶다면 toStrictEqual()로 test를 해줘야 한다.
         name :"Mike",
         age : 30,
     })
 })
+
+/* 
+fn.js
+
+const fn = {
+    add : (num1, num2)=>(num1 + num2),
+    makeUser : (name,age) =>({name, age, gender : undefined}),
+}
+
+module.exports = fn
+*/
