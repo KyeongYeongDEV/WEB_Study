66e7e4981eff7ed788b386f656ddecee8d1e2bc8
KyeongYeongDEV <yeong7098@naver.com>
2023-04-30 16:56:37 +0900

    css 적용

--- Back/NodeJs 채팅/app.js
+++ Back/NodeJs 채팅/app.js
@@ -2,15 +2,27 @@ const express = require("express")
 const socket  = require("socket.io")
 const http = require("http")
 
+const fs = require('fs')
+const { response } = require("express")
 
 const app = express()
 const server = http.createServer(app)
 const io = socket(server)
 
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
 
+//정적인 파일을 제공하기 위해 미들웨어 사용
 app.get("/", (req, res)=>{
-    console.log("유저가 / 으로 접속하였습니다.!")
-    res.send("Hello Express Server!!")
+    fs.readFile('./static/index.html', (err, data)=>{
+        if(err){
+            res.send('Error')
+        }else{
+            res.writeHead(200,{'Content-Type': 'text/html'})
+            res.write(data)
+            response.end()
+        }
+    })
 })
 
 
