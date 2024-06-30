6c59d50f4762b4abd14d5e58cb4f34f25896dd03
KyeongYeongDEV <yeong7098@naver.com>
2024-06-30 17:24:37 +0900

    STUDY : swagger

--- Back/loginLogic/backend/app.ts
+++ Back/loginLogic/backend/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import cors from "cors"
 import index from "./apis/routes/index"
+import swaggerUi from "swagger-ui-express"
+
+const swaggerFile = require("./swagger-output");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(cors())
 
