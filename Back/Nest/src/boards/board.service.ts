deb9a22aeccac2bc65b6579620bfecb6328abfca
KyeongYeongDEV <yeong7098@naver.com>
2024-07-22 23:49:02 +0900

    study : transform Pipes, Dto, Class Validator, RequestBody Validation, Parms Validation, Search Queried Validation, Custom Validation Pipes, Validation with ZOD ( Validation on API Level, Module Level, Global Level)

--- Back/Nest/src/boards/board.service.ts
+++ Back/Nest/src/boards/board.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { BoardEntity } from "./board.entitiy";
 import { BoardsRepository } from "./board.repository";
 import { CreateBoardDto } from "./dto/create-board.dto";
 
@@ -10,7 +11,15 @@ export class BoardService {
         private boardRepostitory : BoardsRepository,
     ){}
 
+    async getBoardById(id : number) : Promise<BoardEntity>{
+        const found = await this.boardRepostitory.findOne(id);
 
+        if(!found){
+            throw new NotFoundeException(`Can't find Board with id ${id}`)
+        }
+
+        return found;
+    }   
     create(createBoardDto : CreateBoardDto){
         return 'This action adds a new board';
     }
