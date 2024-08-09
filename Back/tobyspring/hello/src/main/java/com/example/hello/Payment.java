ce8a36c2c23afba39e9664c8c919c6310fe2226a
KyeongYeongDEV <yeong7098@naver.com>
2024-08-09 17:35:40 +0900

    feat : Payment constructor 및 getter ;

--- Back/tobyspring/hello/src/main/java/com/example/hello/Payment.java
+++ Back/tobyspring/hello/src/main/java/com/example/hello/Payment.java
@@ -1,5 +1,59 @@
 package com.example.hello;
 
-public class Payment {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+public class Payment {
+    private Long orderId;
+    private String currency;
+    // 돈 같은 것은 부동소수점인 Double, Flout으로 하면 액수가 달라질 수 있기 때문에 BigDecimal이라는 타입을 사용한다.
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
 }
