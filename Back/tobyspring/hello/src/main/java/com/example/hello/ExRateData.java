7a1fca44d30caf648b9449ef2ad08a9b88239ffc
KyeongYeongDEV <yeong7098@naver.com>
2024-08-09 20:24:41 +0900

    feat : 실시간 환율을 적용해 환전 계산'

---
+++ Back/tobyspring/hello/src/main/java/com/example/hello/ExRateData.java
@@ -0,0 +1,9 @@
+package com.example.hello;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+}
