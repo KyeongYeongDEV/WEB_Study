183b41e8e2b1f65d006914aedd5001202635dab1
KyeongYeongDEV <yeong7098@naver.com>
2024-08-12 20:15:48 +0900

    feat : Junit을 이용한 test

--- Back/tobyspring/hello/src/main/java/com/example/hello/Client.java
+++ Back/tobyspring/hello/src/main/java/com/example/hello/Client.java
@@ -15,16 +15,6 @@ public class Client {
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
         Payment payment = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
-        System.out.println("payment 1 : "+ payment);
-        System.out.println("-------------\n");
-
-        Payment payment2 = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
-        System.out.println("payment 2 :" + payment2);
-        System.out.println("-------------\n");
-
-        TimeUnit.SECONDS.sleep(3);
-
-        Payment payment3 = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
-        System.out.println("payment 3 :" + payment3);
+        System.out.println("payment : "+ payment);
     }
 }
